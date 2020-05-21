4dc68a378f26b717a49285f65d9ed400bf6130d7
Salvador.Perez <salvador.perez@digitalonus.com>
2020-05-21 15:43:09 -0500

    Added integration code from ecwid platform

--- page.php
+++ page.php
@@ -29,6 +29,21 @@
 <div class="title">
   <h1><?php echo osc_static_page_title(); ?></h1>
 </div>
+<div id="my-search-30045051"></div>
+<div>
+    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?30045051&data_platform=code&data_date=2020-05-21" charset="utf-8"></script>
+    <script type="text/javascript"> xSearch("id=my-search-30045051"); </script>
+</div>
+<div class="ec-cart-widget"></div>
+<div>
+    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?30045051&data_platform=code&data_date=2020-05-21" charset="utf-8"></script>
+    <script type="text/javascript">Ecwid.init();</script>
+</div>
+<div id="my-categories-30045051"></div>
+<div>
+    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?30045051&data_platform=code&data_date=2020-05-21" charset="utf-8"></script>
+    <script type="text/javascript"> xCategoriesV2("id=my-categories-30045051"); </script>
+</div>
 <div id="my-store-30045051"></div>
 <div>
     <script data-cfasync="false"
